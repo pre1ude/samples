6965e7c653ffbab4448588ab9b119324fdd925a2
wxli <wxli@innodealing.com>
2025-06-05 11:18:51 +0800

    feat: update YAxis demo

--- react-sample/src/chart/YAxis.tsx
+++ react-sample/src/chart/YAxis.tsx
@@ -1,20 +1,29 @@
 import React, { useEffect, useState, useRef } from 'react'
-import { init, dispose, Chart, YAxisType } from '@dm/klinecharts'
+import { init, dispose, Chart, YAxisType, YAxisPosition } from '@dm/klinecharts'
 import generatedDataList from '../generatedDataList'
 import Layout from '../Layout'
 
+const positions = [
+  { key: 'right', text: 'Right' },
+  { key: 'left', text: 'Left' }
+]
+
 const types = [
-  { key: 'normal', text: '线性轴' },
-  { key: 'percentage', text: '百分比轴' },
-  { key: 'log', text: '对数轴' }
+  { key: 'normal', text: 'Liner' },
+  { key: 'percentage', text: 'Percentage' },
+  { key: 'log', text: 'Logarithm' }
 ]
 
 export default function CustomThemeKLineChart () {
   const chart = useRef<Chart | null>()
+  const [position, setPosition] = useState('right')
   const [type, setType] = useState('normal')
+  const [isInside, setIsInside] = useState(false)
+  const [isReverse, setIsReverse] = useState(false)
 
   useEffect(() => {
     chart.current = init('y-axis-k-line')
+    chart.current?.createIndicator('VOL', false)
     chart.current?.applyNewData(generatedDataList())
     return () => {
       dispose('y-axis-k-line')
@@ -23,9 +32,27 @@ export default function CustomThemeKLineChart () {
 
   useEffect(() => {
     chart.current?.setStyles({
+      yAxis: { position: position as YAxisPosition }
+    })
+  }, [position])
+
+  useEffect(() => {
+    chart.current?.setStyles({
       yAxis: { type: type as YAxisType }
     })
   }, [type])
+
+  useEffect(() => {
+    chart.current?.setStyles({
+      yAxis: { inside: isInside  }
+    })
+  }, [isInside])
+
+  useEffect(() => {
+    chart.current?.setStyles({
+      yAxis: { reverse: isReverse  }
+    })
+  }, [isReverse])
 
   return (
     <Layout
@@ -35,17 +62,34 @@ export default function CustomThemeKLineChart () {
         className="k-line-chart"/>
       <div
         className="k-line-chart-menu-container">
-        {
-          types.map(({ key, text }) => {
-            return (
-              <button
-                key={key}
-                onClick={_ => { setType(key) }}>
-                {text}
-              </button>
-            )
-          })
-        }
+        <label>
+          内部:
+          <input type='checkbox' checked={isInside} onChange={(e) => setIsInside(e.target.checked)}/>
+        </label>
+        <label>
+          反转:
+          <input type='checkbox' checked={isReverse} onChange={(e) => setIsReverse(e.target.checked)}/>
+        </label>
+        <p>
+          位置:
+          {
+            positions.map(({ key, text }) => {
+              return (
+                <label><input type="radio" name="position" value={key} checked={key === position} onChange={(e) => setPosition(e.target.value)} />{text}</label>
+              )
+            })
+          }
+        </p>
+        <p>
+          类型:
+          {
+            types.map(({ key, text }) => {
+              return (
+                <label><input type="radio" name="type" value={key} checked={key === type} onChange={(e) => setType(e.target.value)} />{text}</label>
+              )
+            })
+          }
+        </p>
       </div>
     </Layout>
   )
